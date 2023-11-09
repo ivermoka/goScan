6c59a9c9f81090ff1a794f3a262683871c61e184
Iver <ivermoka@icloud.com>
2023-11-09 13:46:12 +0100

    getLatestGoVersion function added

--- goScan.go
+++ goScan.go
@@ -5,14 +5,20 @@ package main
 // go repos with go version >1.11 will be shown as >1.11 (version is not in go.mod)
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
 )
 
 func main() {
+	var bvar bool
+	flag.BoolVar(&bvar, "l", false, "latest")
+	flag.Parse()
 	f, err := os.Open(".")
 	if err != nil {
 		fmt.Println(err)
@@ -26,6 +32,7 @@ func main() {
 	if len(files) == 0 {
 		fmt.Fprintf(os.Stderr, "%s\n", "No files found.")
 	}
+	latestVersion := getLatestGoVersion()
 	for _, v := range files {
 		if !v.IsDir() {
 			continue
@@ -43,7 +50,9 @@ func main() {
 			var goVersion string
 			if hasGoVersion(string(b)) {
 				goVersion = getGoVersion(v.Name())
-				os.Args = append(os.Args, "1.21.4")
+				if bvar {
+					os.Args = append(os.Args, latestVersion)
+				}
 				isArg := false
 				for _, args := range os.Args[1:] {
 					if goVersion == string(args) {
@@ -112,4 +121,29 @@ func getDockerFrom(pt string) string {
 		return "correct version"
 	}
 	return trimmed
+}
+
+func getLatestGoVersion() string {
+	const filteredContentStart = 20
+	const filteredContentLength = 128
+	const goRemover = 6
+	res, err := http.Get("https://go.dev/dl/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := string(body)
+	indexStable := strings.Index(strings.ToLower(content), "id=\"stable\"")
+	filteredContent := content[indexStable+filteredContentStart:indexStable+filteredContentLength]
+	indexVersion := strings.Index(filteredContent, "id=\"")
+	indexVersionEnd := strings.Index(filteredContent, "\">")
+	version := filteredContent[indexVersion+goRemover:indexVersionEnd]
+	return version
 }
